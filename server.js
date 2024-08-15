fbedd31473199476b9d8500258f5bfca16399b48
Joseph <kamoldinovq@gmail.com>
2024-08-15 18:03:02 +0900

    feat: author ejs portfolio publish

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ console.log("Web Serverni boshlash");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
 
 // 1: Kirish code
 app.use(express.static("public"));
@@ -25,8 +35,13 @@ app.set("view engine", "ejs");
 //     res.render('project');
 //   });
 app.post("/create-item", (req, res) => {
-  console.log(req);
-  res.json({ test: "success" });
+  //   console.log(req);
+  //   res.json({ test: "success" });
+  //  TODO: code with db here
+});
+
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
 });
 
 app.get("/", function (req, res) {
